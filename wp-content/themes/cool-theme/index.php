133b1a7033c0867ac861ffff419d3bd443ac6ef0
Marcos Amorim <markstm10@gmail.com>
2022-04-12 17:35:43 -0300

    feat: creates page initial structure on index.php

---
+++ wp-content/themes/cool-theme/index.php
@@ -0,0 +1,43 @@
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cool Theme</title>
+</head>
+<body <?php body_class();?>>
+    <p>Hello World!</p>
+
+    <header>
+        <div class="top-bar">
+            <div class="social-networks"></div>
+            <div class="search"></div>
+        </div>
+        <div class="menu-area">
+            <div class="logo">
+
+            </div>
+            <div class="nav-menu"></div>
+        </div>
+    </header>
+
+    <div class="content">
+        <main>
+            <section class="slide"></section>
+            <section class="services"></section>
+            <section class="middle">
+                <aside class="sidebar">
+
+                </aside>
+                <div class="news"></div>
+            </section>
+
+            <section class="map"></section>
+        </main>
+    </div>
+
+    <footer>
+        
+    </footer>
+</body>
+</html>
