fc98a8dac0a9dfc203ed7dfe4a3acaf6b646f276
Marcos Amorim <markstm10@gmail.com>
2022-04-12 17:46:22 -0300

    feat: separates the basic page structure blocks

--- wp-content/themes/cool-theme/index.php
+++ wp-content/themes/cool-theme/index.php
@@ -1,26 +1,4 @@
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cool Theme</title>
-</head>
-<body <?php body_class();?>>
-    <p>Hello World!</p>
-
-    <header>
-        <div class="top-bar">
-            <div class="social-networks"></div>
-            <div class="search"></div>
-        </div>
-        <div class="menu-area">
-            <div class="logo">
-
-            </div>
-            <div class="nav-menu"></div>
-        </div>
-    </header>
-
+    <?php get_header();?>
     <div class="content">
         <main>
             <section class="slide"></section>
@@ -36,8 +14,4 @@
         </main>
     </div>
 
-    <footer>
-        
-    </footer>
-</body>
-</html>
+    <?php get_footer(); ?>
